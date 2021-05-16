45e52046b442b14ac157ff9119a5635c7e9d9eee
mayumi04 <bbpcb373@yahoo.co.jp>
2021-05-16 22:06:54 +0900

    性別を真偽値にした

--- kadai301_02.py
+++ kadai301_02.py
@@ -1,16 +1,23 @@
-class Cow():
-    def __init__(self, name, age, gender, weight):
+class Cow:
+    def __init__(self, name:str, age:int, gender:bool, weight:float):
         self.name = name
         self.age = age
         self.gender = gender
         self.weight = weight
 
+        if gender:
+            self.gender = 'メス'
+        else:
+            self.gender = 'オス'
+
     def talk(self):
         print(f'{self.name} {self.age}歳 {self.gender} {self.weight}kg')
 
-hanako = Cow('はなこ', 3, 'メス', 100.4)
-tarou = Cow('たろう', 4, 'オス', 200.5)
-mugen = Cow('むげん', 9, 'オス', 300.6)
+
+hanako = Cow('はなこ', 3, 1, 100.4)
+tarou = Cow('たろう', 4, 0, 200.5)
+mugen = Cow('むげん', 9, 0, 300.6)
+
 hanako.talk()
 tarou.talk()
 mugen.talk()
