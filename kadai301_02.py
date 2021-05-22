6df19e4740170d955eb636c9dfa6b1b06f2ad4cc
mayumi04 <bbpcb373@yahoo.co.jp>
2021-05-22 19:48:01 +0900

    性別の型をそろえた

--- kadai301_02.py
+++ kadai301_02.py
@@ -5,18 +5,17 @@ class Cow:
         self.gender = gender
         self.weight = weight
 
-        if gender:
-            self.gender = 'メス'
-        else:
-            self.gender = 'オス'
-
     def talk(self):
-        print(f'{self.name} {self.age}歳 {self.gender} {self.weight}kg')
+        if self.gender:
+            gender_name = 'メス'
+        else:
+            gender_name = 'オス'
+        print(f'{self.name} {self.age}歳 {gender_name} {self.weight}kg')
 
 
-hanako = Cow('はなこ', 3, 1, 100.4)
-tarou = Cow('たろう', 4, 0, 200.5)
-mugen = Cow('むげん', 9, 0, 300.6)
+hanako = Cow('はなこ', 3, True, 100.4)
+tarou = Cow('たろう', 4, False, 200.5)
+mugen = Cow('むげん', 9, False, 300.6)
 
 hanako.talk()
 tarou.talk()
